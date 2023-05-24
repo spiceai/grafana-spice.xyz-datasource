00b78c58f68b2faf7e850260f7942dc60b64225a
Evgenii Khramkov <evgenii@spice.ai>
2023-05-24 15:59:08 +0400

    fix: update magefile and ci for supported platforms

--- Magefile.go
+++ Magefile.go
@@ -6,7 +6,14 @@ package main
 import (
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/mg"
 )
 
+func BuildCustom() {
+	b := build.Build{}
+	// same as build.BuildAll, but without Windows and LinuxARM - apache/arrow/go/v10 has problems with compilation on that platforms
+	mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64, b.GenerateManifestFile)
+}
+
 // Default configures the default target.
-var Default = build.BuildAll
+var Default = BuildCustom
